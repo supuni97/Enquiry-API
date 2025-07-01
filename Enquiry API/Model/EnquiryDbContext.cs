2c8cc9f17baa9189953805cd09f986d905c30fb8
supuni97 <sitharabandara1997@gmail.com>
2025-07-01 12:51:15 +0530

    Created EnquiryDbContext class

---
+++ Enquiry API/Model/EnquiryDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Enquiry_API.Model
+{
+    public class EnquiryDbContext:DbContext
+    {
+        public EnquiryDbContext(DbContextOptions<EnquiryDbContext> options):base(options)
+        {
+
+        }
+    }
+}
--- Enquiry API/Program.cs
+++ Enquiry API/Program.cs
@@ -1,11 +1,13 @@
+using Enquiry_API.Model;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<EnquiryDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("EnquiryCon")));
 
 var app = builder.Build();
 
